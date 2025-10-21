22972204c24cd03b030969598f2d3a9776c1b57b
Horia Delicoti <horia.delicoti@gmail.com>
2025-10-21 16:44:31 +0100

    Docusaurus config: Add docusaurus config file

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Knowledge Wiki 🌱',
+  tagline: 'Welcome to my personal knowledge wiki',
+  favicon: 'img/profile-favicon.png',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  url: 'https://wiki.horia.delicoti.com', // Set the production url of your site here
+
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'horia-delicoti', // Usually your GitHub org/user name.
+  projectName: 'wiki', // Usually your repo name.
+
+  onBrokenLinks: 'throw', // Throw an error on broken links
+  onBrokenMarkdownLinks: 'warn', // Warn on broken markdown links
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang.
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  customFields: {
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.js',
+        },
+        blog: false, // Disable the blog plugin
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Knowledge Wiki 🌱',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/profile.jpeg',
+        },
+        items: [
+          {
+            href: 'https://github.com/horia-delicoti/wiki',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
